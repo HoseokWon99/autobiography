a8ecb10a4daf7b2f7f0e998a07e274d6899bbc5f
hoseok <yamae2468@naver.com>
2023-11-27 00:31:46 +0900

    proto

---
+++ prototype/functions/src/handler/mail.ts
@@ -0,0 +1,8 @@
+import { firestore } from "../config/firebase";
+import { Mail } from "../model/mail";
+
+const mailColl = firestore.collection("mail");
+
+export const sendMailHandler = async (mail: Mail)=> {
+  await mailColl.add(mail);
+};
